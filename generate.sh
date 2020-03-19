f6a0fedffd1a8b0fad9b07a3073e560ba23d7972
Marouane BENALLA <marouane.benalla@gmail.com>
2020-03-19 02:34:59 +0100

    Initial commit

--- generate.sh
+++ generate.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Copy blog content
-wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368
+wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://benallamar.github.io
 
 # Copy 404 page
-wget --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links --content-on-error --timestamping http://localhost:2368/404.html
+wget --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links --content-on-error --timestamping http://benallamar.github.io/404.html
 
 # Copy sitemaps
-wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap.xsl
-wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap.xml
-wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-pages.xml
-wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-posts.xml
-wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-authors.xml
-wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://localhost:2368/sitemap-tags.xml
+wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://benallamar.github.io/sitemap.xsl
+wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://benallamar.github.io/sitemap.xml
+wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://benallamar.github.io/sitemap-pages.xml
+wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://benallamar.github.io/sitemap-posts.xml
+wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://benallamar.github.io/sitemap-authors.xml
+wget --recursive --no-host-directories  --adjust-extension --timeout=30 --no-parent --convert-links http://benallamar.github.io/sitemap-tags.xml
 
 # Replace localhost with domain
-LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e 's/http:\/\/localhost:2368/http:\/\/benallamar.github.io/g' {} +
-LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e 's/localhost:2368/benallamar.github.io/g' {} +
-LC_ALL=C find ./static -type f -not -wholename *.git* -exec sed -i '' -e 's/http:\/\/www.gravatar.com/https:\/\/www.gravatar.com/g' {} +
+LC_ALL=C find . -type f -not -wholename *.git* -exec sed -i '' -e 's/http:\/\/benallamar.github.io/http:\/\/benallamar.github.io/g' {} +
+LC_ALL=C find . -type f -not -wholename *.git* -exec sed -i '' -e 's/benallamar.github.io/benallamar.github.io/g' {} +
+LC_ALL=C find . -type f -not -wholename *.git* -exec sed -i '' -e 's/http:\/\/www.gravatar.com/https:\/\/www.gravatar.com/g' {} +
 
 # Set up Github Pages CNAME
 echo "benallamar.github.io" > CNAME
